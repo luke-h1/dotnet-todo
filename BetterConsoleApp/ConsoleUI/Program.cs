0746e399d495fb8293d8406ec792cf4d82eab4aa
luke-h1 <luke.howsam@yahoo.com>
2021-07-21 13:19:32 +0100

    setup logging

--- BetterConsoleApp/ConsoleUI/Program.cs
+++ BetterConsoleApp/ConsoleUI/Program.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+// DI, serilog & Settings
+
 
 namespace ConsoleUI
 {
@@ -6,7 +14,44 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+                Log.Logger.Information("Application Starting");
+                var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) => 
+                {
+
+                }).UseSerilog().Build();
+        }
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                  .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                  .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+                  .AddEnvironmentVariables();
+
+        }
+    }
+    public class GreetingService 
+    {
+        private readonly ILogger<GreetingService> _log; 
+        public GreetingService(ILogger<GreetingService> log, IConfiguration config)
+        {
+            _log = log; 
+        }
+        public void Run() 
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                
+            }
         }
     }
 }
